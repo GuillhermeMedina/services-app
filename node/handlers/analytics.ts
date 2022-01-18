fbc977e3851d61ff87747159d8a67d573693bc76
GuilhermeMedina <guilherme.de.b.medina@gmail.com>
2022-01-18 16:53:29 -0300

    Docs: Atualização no app

--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,42 +1,48 @@
-
 const axios = require('axios');
 
 
 export async function analytics(ctx: Context, next: () => Promise<any>) {
-  const url = 'https://lojabuettner.vtexcommercestable.com.br/api/oms/pvt/orders/1203221251139-01';
+  const paramId = getParamsByURL(ctx.request.url);
+
+  const url = `https://lojabuettner.vtexcommercestable.com.br/api/oms/pvt/orders/${paramId}`;
 
   const options = {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'X-Vtex-Use-Https': 'true',
-    'Content-Type': 'application/json',
-    'X-VTEX-API-AppKey': 'vtexappkey-lojabuettner-SZWOKM',
-    'X-VTEX-API-AppToken': 'ARSDEOKLLHNUMAPSUXXIXKVLLPOMPVQRLTTLFAPLRYPMANJLIJESOPRPWGROAXWPQVJSGDNUHMRZVILYGCEFRXDVCEPYIBKTCZVEDQFEVJKCYRTNVTIYSQXTSOQCOOLG'
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'X-Vtex-Use-Https': 'true',
+      'Content-Type': 'application/json',
+      'X-VTEX-API-AppKey': 'vtexappkey-lojabuettner-SZWOKM',
+      'X-VTEX-API-AppToken': 'ARSDEOKLLHNUMAPSUXXIXKVLLPOMPVQRLTTLFAPLRYPMANJLIJESOPRPWGROAXWPQVJSGDNUHMRZVILYGCEFRXDVCEPYIBKTCZVEDQFEVJKCYRTNVTIYSQXTSOQCOOLG'
+    } 
+  };
+
+  async function getOrder() {
+    const data = await axios(url, options)
+    .then((res: { json: () => any; }) => {
+      //@ts-ignore;
+        const data = res.data;
+        
+        return data;
+    })
+    .catch((err: string) => console.error('error:' + err));
+    
+    return data
   }
-};
-
-async function getOrder() {
-  const data = await axios(url, options)
-  .then((res: { json: () => any; }) => {
-    //@ts-ignore;
-      const data = res.data;
-      
-      return data;
-  })
-  .catch((err: string) => console.error('error:' + err));
-  
-  return data
-}
  
-  ctx.set('cache-control', 'no-cache')
-  ctx.set('Access-Control-Allow-Origin', '*')
-
+  ctx.set('cache-control', 'no-cache');
+  ctx.set('Access-Control-Allow-Origin', '*');
+  
   const listInfo = await getOrder();
-  //@ts-ignore;
+
   ctx.body = listInfo;
   await next()
 }
 
+function getParamsByURL(currentURL: string) {
+  const urlParams = new URLSearchParams(currentURL);
 
+  const orderId = urlParams.get('/_v/app/analytics/realTime/orderId');
 
+  return orderId;
+}
--- node/index.ts
+++ node/index.ts
@@ -17,9 +17,6 @@ import { updateLiveUsers } from './event/liveUsersUpdate'
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
 metrics.trackCache('status', memoryCache)
 
-const TREE_SECONDS_MS = 3 * 1000
-const CONCURRENCY = 10
-
 declare global {
   type Context = ServiceContext<Clients, State>
 
@@ -29,23 +26,6 @@ declare global {
 }
 
 export default new Service<Clients, State, ParamsContext>({
-  clients: {
-    implementation: Clients,
-    options: {
-      default: {
-        retries: 2,
-        timeout: 10000,
-      },
-      events: {
-        exponentialTimeoutCoefficient: 2,
-        exponentialBackoffCoefficient: 2,
-        initialBackoffDelay: 50,
-        retries: 1,
-        timeout: TREE_SECONDS_MS,
-        concurrency: CONCURRENCY,
-      },
-    },
-  },
   routes: {
     analytics: method({
       GET: [analytics],
